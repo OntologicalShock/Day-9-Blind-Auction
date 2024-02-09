455f87f65280cf1c98ba0527770b7dcb36eac45c
OntologicalShock <58638318+OntologicalShock@users.noreply.github.com>
2024-02-09 19:43:23 +0000

    Completed program.

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-import time
 from replit import clear
-print("test")
-time.sleep(3)
-clear()
+import art
+
+
+bidder = ""
+bid = int(0)
+bidDB = {}
+anotherBidder = "yes"
+
+def bidLoop():
+    global bidder
+    global bid
+    global bidDB
+    global anotherBidder
+    
+    print(art.logo)
+    bidder = input("what's your name?\n")
+    bid = int(input("What's your bid in $?\n"))
+    bidDB[bidder] = bid
+    anotherBidder = input("Is there another bidder? (y/n)")
+
+bidLoop()
+while anotherBidder == "y":
+    clear()
+    # print(bidDB) #Bit of debugging, ignore.
+    bidLoop()
+
+winBidder = max(bidDB, key=bidDB.get)
+winBid = bidDB[winBidder]
+print(f"The winner is {winBidder} for ${winBid}!")
